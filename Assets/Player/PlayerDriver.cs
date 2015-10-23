789d14b74e873c401cf4de4d11523f85a24d9cb7
Jacob Hunter <jacob.m.hunter.99@gmail.com>
2015-10-23 17:34:35 -0400

    Added PlayerStats

---
+++ Assets/Player/PlayerDriver.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDriver : MonoBehaviour {
+    public int health = 10;
+    public int points = 0;
+    public int money = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
