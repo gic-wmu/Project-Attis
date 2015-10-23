835d1528e9a02ba850fd041b6f5981b1c4cf9000
Jacob Hunter <jacob.m.hunter.99@gmail.com>
2015-10-22 21:55:43 -0400

    The Tower will now repeatedly shoot at selected time delay

--- Assets/Tower/PeaTower.cs
+++ Assets/Tower/PeaTower.cs
@@ -3,23 +3,48 @@ using System.Collections;
 
 public class PeaTower : MonoBehaviour {
     public GameObject bulletObject;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+    public float timeDelay = 1;
+    private Collider target;
 	// Update is called once per frame
 	void Update () {
-	
-	}
+       
+    }
 
     void OnTriggerEnter(Collider co)
     {
-        if(co.tag == "Creep")
+
+        if ((co.tag == "Creep") && !IsInvoking("Shoot"))
         {
-            GameObject g = (GameObject)Instantiate(bulletObject, transform.position, transform.rotation);
-            g.GetComponent<Bullet>().target = co.transform;
+            target = co;
+            Invoke("Shoot", 0);
+        }
+
+
+    }
+
+    void OnTriggerLeave(Collider co)
+    {
+        if(co == target)
+        {
+            StopCoroutine("Shoot");
         }
         
+    }
+
+    void OnTriggerStay(Collider co)
+    {
+        if ((co.tag == "Creep") && !IsInvoking("Shoot"))
+        {
+            target = co;
+            Invoke("Shoot", timeDelay);
+            //GameObject g = (GameObject)Instantiate(bulletObject, transform.position, Quaternion.identity);
+            //g.GetComponent<Bullet>().target = co.transform;
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject g = (GameObject)Instantiate(bulletObject, transform.position, Quaternion.identity);
+        g.GetComponent<Bullet>().target = target.transform;
     }
 }
